39442721739d2543f24c1d41d8d11d2c98468155
Biruk <mengi024@morris.umn.edu>
2021-09-05 15:11:28 -0500

    got R0 to work

---
+++ regex/regex.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+R0=$(sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r0_input.txt)
+#R1=$(sed -E 's/\* I am ([a-zA-Z\s]{1,})\. My favorite sandwich is ([a-zA-Z\s]{1,})./1. \1\n2. \2\n/' < r1_input.txt)
+#R2=$(sed -E 's/\* ([a-zA-Z]+), ([a-zA-Z]+)/1. \1\n2. \2\n/' < r2_input.txt)
+echo "$R0" >> r0_output.txt
+#echo "$R1" >> r1_output.txt
+#echo "$R2" >> r2_output.txt
